549ba3a24202aa1801c785ebaacab91ea68c5746
Amin <asaa47721@gmail.com>
2021-10-15 23:52:57 +0330

    install laravel backpack package. create controllers and requests for models

---
+++ app/Http/Controllers/Admin/AttributeProductCrudController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\AttributeProductRequest;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+
+/**
+ * Class AttributeProductCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class AttributeProductCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+    // use \Backpack\CRUD\app\Http\Controllers\Operations\InlineCreateOperation;
+
+
+    /**
+     * Configure the CrudPanel object. Apply settings to all operations.
+     * 
+     * @return void
+     */
+    public function setup()
+    {
+        CRUD::setModel(\App\Models\AttributeProduct::class);
+        CRUD::setRoute(config('backpack.base.route_prefix') . '/attribute-product');
+        CRUD::setEntityNameStrings('attribute product', 'attribute product');
+
+        // $this->crud->with(['attribute', 'product']);
+    }
+
+    /**
+     * Define what happens when the List operation is loaded.
+     * 
+     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+     * @return void
+     */
+    protected function setupListOperation()
+    {
+        CRUD::column('product');
+        CRUD::column('attribute');
+        CRUD::column('value');
+
+        /**
+         * Columns can be defined using the fluent syntax or array syntax:
+         * - CRUD::column('price')->type('number');
+         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
+         */
+    }
+
+    /**
+     * Define what happens when the Create operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-create
+     * @return void
+     */
+    protected function setupCreateOperation()
+    {
+        CRUD::setValidation(AttributeProductRequest::class);
+
+        CRUD::field('product_id')
+            ->type('select2')
+            ->attribute('name')
+            ->model(\App\Models\Product::class);
+
+        CRUD::field('attribute_id')
+            ->type('select2')
+            ->attribute('name')
+            ->model(\App\Models\Attribute::class);
+
+        CRUD::field('value');
+
+        /**
+         * Fields can be defined using the fluent syntax or array syntax:
+         * - CRUD::field('price')->type('number');
+         * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
+         */
+    }
+
+    /**
+     * Define what happens when the Update operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-update
+     * @return void
+     */
+    protected function setupUpdateOperation()
+    {
+        $this->setupCreateOperation();
+    }
+
+
+    protected function setupShowOperation()
+    {
+        CRUD::column('product');
+        CRUD::column('attribute');
+        CRUD::column('value');
+    }
+}
---
+++ app/Http/Controllers/Admin/ProductCrudController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\ProductRequest;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+
+/**
+ * Class ProductCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class ProductCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+    /**
+     * Configure the CrudPanel object. Apply settings to all operations.
+     * 
+     * @return void
+     */
+    public function setup()
+    {
+        CRUD::setModel(\App\Models\Product::class);
+        CRUD::setRoute(config('backpack.base.route_prefix') . '/product');
+        CRUD::setEntityNameStrings('product', 'products');
+    }
+
+    /**
+     * Define what happens when the List operation is loaded.
+     * 
+     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+     * @return void
+     */
+    protected function setupListOperation()
+    {
+        CRUD::column('name');
+        CRUD::column('slug');
+        CRUD::column('description');
+        CRUD::column('price');
+        CRUD::column('stock');
+        CRUD::column('image');
+        CRUD::column('published')->type('boolean');
+        
+        /**
+         * Columns can be defined using the fluent syntax or array syntax:
+         * - CRUD::column('price')->type('number');
+         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
+         */
+    }
+
+    /**
+     * Define what happens when the Create operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-create
+     * @return void
+     */
+    protected function setupCreateOperation()
+    {
+        CRUD::setValidation(ProductRequest::class);
+
+        CRUD::field('name');
+        CRUD::field('slug');
+        CRUD::field('description');
+        CRUD::field('category_id');
+        CRUD::field('price');
+        CRUD::field('stock');
+        CRUD::field('image')->type('image');
+        CRUD::field('published');
+
+
+        CRUD::field('attributes')
+            ->label('Attributes')
+            ->type('relationship')
+            // ->name('attributes')
+            // ->entity('attribute')
+            // ->attribute('name')
+            // ->model(\App\Models\Attribute::class)
+            // ->pivot(true)
+            ->tab('Relationships')
+            ->inline_create([
+                'entity' => 'attribute', // the entity in singular
+                // OPTIONALS
+                'force_select' => true, // should the inline-created entry be immediately selected?
+                'modal_class' => 'modal-dialog modal-xl', // use modal-sm, modal-lg to change width
+                'modal_route' => route('attribute-inline-create'), // InlineCreate::getInlineCreateModal()
+                'create_route' =>  route('attribute-inline-create-save'), // InlineCreate::storeInlineCreate()
+                'include_main_form_fields' => ['field1', 'field2'], // pass certain fields from the main form to the modal
+
+            ])
+            ->ajax(true)
+        ;
+
+            // 'model'     => "Backpack\NewsCRUD\app\Models\Tag", // foreign key model
+            // 'pivot'     => true, // on create&update, do you need to add/delete pivot table entries?
+            // 'tab'       => 'Relationships',
+
+        /**
+         * Fields can be defined using the fluent syntax or array syntax:
+         * - CRUD::field('price')->type('number');
+         * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
+         */
+    }
+
+    /**
+     * Define what happens when the Update operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-update
+     * @return void
+     */
+    protected function setupUpdateOperation()
+    {
+        $this->setupCreateOperation();
+    }
+}
