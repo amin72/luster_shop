186502038472837bbf8690a2010b39ebc5e5f176
Amin <asaa47721@gmail.com>
2022-01-18 17:27:07 +0330

    add links for top products and random products

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -30,11 +30,13 @@
       {{-- products section --}}
       <div class="flex justify-between items-center pt-2 pb-8 text-sm xl:text-base flex-wrap">
         @foreach (\App\Models\TopProduct::where('is_active', true)->orderBy('position')->take(6)->get() as $product)
-          <div class="w-1/2 md:w-1/3 lg:w-1/6 text-center hover:shadow-xl px-1 sm:px-2 py-4">
+          <a
+            href="{{ route('products.show', $product->product->slug) }}"
+            class="w-1/2 md:w-1/3 lg:w-1/6 text-center hover:shadow-xl px-1 sm:px-2 py-4">
             <img class="mb-4 rounded h-full w-full mx-auto" src="{{ asset($product->product->image) }}" alt="{{ $product->product->name }}">
             <span class="text-ceter text-gray-500 mx-auto">{{ $product->product->name }}</span>
             <div class="text-left ml-2 text-gray-500 mt-6">{{ $product->product->price }} تومان</div>
-          </div>
+          </a>
         @endforeach
       </div>
     </section>
@@ -43,18 +45,20 @@
     {{-- luster products --}}
     <section class="rounded-lg bg-white shadow w-full py-4 px-6">
       {{-- header --}}
-      <div class="flex justify-between font-bold text-lg border-b mb-4">
+      <div class="flex justify-between font-bold text-lg border-b mb-2">
         <span class="py-4 border-b border-red-500 -my-px text-gray-800 font-semibold tracking-wide">دیگر محصولات</span>
       </div>
   
       {{-- products section --}}
-      <div class="flex justify-between items-center pt-2 pb-8 text-sm xl:text-base flex-wrap">
+      <div class="flex justify-between items-center pb-8 text-sm xl:text-base flex-wrap">
         @foreach (\App\Models\RandomProduct::where('is_active', true)->orderBy('position')->take(30)->get() as $product)
-          <div class="w-1/2 md:w-1/3 lg:w-1/6 text-center hover:shadow-xl px-1 sm:px-2 py-4">
+          <a
+            href="{{ route('products.show', $product->product->slug) }}"
+            class="w-1/2 md:w-1/3 lg:w-1/6 text-center hover:shadow-xl px-1 sm:px-2 py-8">
             <img class="mb-4 rounded h-full w-full mx-auto" src="{{ asset($product->product->image) }}" alt="{{ $product->product->name }}">
             <span class="text-ceter text-gray-500 mx-auto">{{ $product->product->name }}</span>
             <div class="text-left ml-2 text-gray-500 mt-6">{{ $product->product->price }} تومان</div>
-          </div>
+          </a>
         @endforeach
       </div>
     </section>  
